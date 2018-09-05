a8fdc9d73020a9a6c4b8cf4c1132a0a28bcd5fd0
galileoguzman <galileoguzman@gmail.com>
2018-09-05 17:40:26 -0500

    Download PDF file with Objective C
    - Using UIDocumentInteractionController

--- PDFDownload/ViewController.h
+++ PDFDownload/ViewController.h
@@ -8,8 +8,10 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<UIDocumentInteractionControllerDelegate>
+@property (weak, nonatomic) IBOutlet UIButton *btnDownload;
 
+- (IBAction)btnDownloadPressed:(id)sender;
 
 @end
 
--- PDFDownload/ViewController.m
+++ PDFDownload/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic, strong) UIDocumentInteractionController* documentInteractionController;
 @end
 
 @implementation ViewController
@@ -26,4 +26,76 @@
 }
 
 
+- (IBAction)btnDownloadPressed:(id)sender {
+    
+    self.btnDownload.enabled = NO;
+    
+    [self.btnDownload setTitle:@"Loading ..." forState:UIControlStateDisabled];
+    
+    [self initDownloadFile];
+}
+
+
+-(void) initDownloadFile{
+    
+    
+    // UNIQUE NAME
+    NSTimeInterval timeStamp = [[NSDate date] timeIntervalSince1970];
+    NSNumber *timeStampObj = [NSNumber numberWithDouble: timeStamp];
+    
+    // GET STRING NAME FOR SANDBOX
+    NSString* documentDir = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) firstObject];
+    
+    // FILE NAME
+    NSString* stFilePath = [documentDir stringByAppendingPathComponent:[NSString stringWithFormat:@"%@.pdf",timeStampObj]];
+    
+    // NEW REQUEST OBJ
+    NSURLRequest *request = [NSURLRequest requestWithURL:[NSURL URLWithString:@"https://www.bowers-wilkins.co.uk/Admin/Downloads/Product/Support/Zeppelin/ENG_FP28495_Zeppelin_Connectivity.pdf"]];
+    
+    [NSURLConnection sendAsynchronousRequest:request queue:[NSOperationQueue currentQueue] completionHandler:^(NSURLResponse *response, NSData *data, NSError *error) {
+        if (error) {
+            NSLog(@"ERROR HAPPENED %@", error);
+        }
+        if (data) {
+            [data writeToFile:stFilePath atomically:YES];
+            
+            [self showFileDownloadedWithPath:stFilePath];
+        }
+    }];
+}
+
+-(void)showFileDownloadedWithPath:(NSString*)pathFile{
+    NSURL *URL = [NSURL fileURLWithPath:pathFile];
+    
+    
+    self.btnDownload.enabled = YES;
+    [self.btnDownload setTitle:@"Download" forState:UIControlStateNormal];
+    
+    if (URL) {
+        // Initialize Document Interaction Controller
+        self.documentInteractionController = [UIDocumentInteractionController interactionControllerWithURL:URL];
+        
+        // Configure Document Interaction Controller
+        [self.documentInteractionController setDelegate:self];
+        
+        // Preview PDF
+        [self.documentInteractionController presentPreviewAnimated:YES];
+    }
+}
+
+# pragma mark - UIDocumentInteractionControllerDelegate
+- (UIViewController *)documentInteractionControllerViewControllerForPreview:(UIDocumentInteractionController *)controller
+{
+    return self;
+}
+
+- (UIView *)documentInteractionControllerViewForPreview:(UIDocumentInteractionController *)controller
+{
+    return self.view;
+}
+
+- (CGRect)documentInteractionControllerRectForPreview:(UIDocumentInteractionController *)controller
+{
+    return self.view.frame;
+}
 @end
