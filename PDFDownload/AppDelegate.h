b0842e17e2e9b1cca6db7803fbcb3748851f7b47
galileoguzman <galileoguzman@gmail.com>
2018-09-05 17:16:32 -0500

    Initial Commit

---
+++ PDFDownload/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PDFDownload
+//
+//  Created by Galileo Guzman on 9/5/18.
+//  Copyright © 2018 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PDFDownload/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PDFDownload
+//
+//  Created by Galileo Guzman on 9/5/18.
+//  Copyright © 2018 Galileo Guzman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
